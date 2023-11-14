c35c8c5dc62f0f79ad91b3109e247e5b40784d63
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-11-14 00:56:52 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
